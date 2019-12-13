773f9b5fdaa3e3c027776a3623eecd53935b34a3
Alexandra Hermeneanu <ahermeneanu@esdoffice.local>
2019-12-13 15:42:25 +0200

    added DbContext

---
+++ DataMapper/LibraryDBContext.cs
@@ -0,0 +1,24 @@
+﻿using DomainModel;
+using System.Data.Entity;
+
+namespace DataMapper
+{
+    public class LibraryDBContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LibraryDBContext"/> class.
+        /// </summary>
+        public LibraryDBContext() : base("DatabaseConnectionString")
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the books.
+        /// </summary>
+        /// <value>
+        /// The customers.
+        /// </value>
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
